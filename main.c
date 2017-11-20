b73aa2685227c75ea829e2ba1eea07abadaff65a
zbyszek <zbyszek@XPS>
2017-11-20 21:25:44 +0100

    Some cleanup

--- main.c
+++ main.c
@@ -18,10 +18,6 @@
 #define LED_PIN PB5
 #define LED_TOGGLE() (LED_PORT ^= 1<<LED_PIN)
 
-#define BUFF_SIZE 16
-char buff[BUFF_SIZE];	//Buffer for serial data
-uint8_t BI = 0;			//Buffer Index
-
 int main()
 {
 	initUART();
@@ -30,8 +26,6 @@ int main()
 	while(1)
 	{
 		//main loop
-//		writeString(buff);
-//		_delay_ms(500);
 	}
 }
 ISR(USART_RX_vect)
@@ -41,27 +35,10 @@ ISR(USART_RX_vect)
 	switch(c)
 	{
 	case 13://CR
-		if(BI==0) break;// you you didn't enter anything, don't do anything.
 		writeString("\r\n");
-		//do sth with entered data
-		writeString("\e[1;1H\e[2J"); //clear console
-		char msgBuff[30];
-		sprintf(msgBuff, "You entered: %s\n\rType in the speed:", buff);
-		writeString(msgBuff);
-		//
-		memset(buff,0 , sizeof(buff));// "zero" the buffer
-		BI = 0;
-		break;
-	case 48 ... 57:
-		if(BI < BUFF_SIZE-1)//leave 1 byte for trailing \n
-		{
-			putByte(c);
-			buff[BI++] = c;
-		}
-		//else putByte((char)'X');
 		break;
 	default:
+		putByte((char)'X');
 		break;
-
 	}
 }
